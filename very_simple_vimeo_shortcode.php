54960d715add19ddde451f74c7d666abc33f4fcc
yourlocalwebmaster <yourlocalwebmaster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-10 00:03:08 +0000

    Updated plugin GPL2
    git-svn-id: https://plugins.svn.wordpress.org/the-very-simple-vimeo-shortcode/trunk@517070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- very_simple_vimeo_shortcode.php
+++ very_simple_vimeo_shortcode.php
@@ -10,7 +10,7 @@ License: GPL2
 */
 ?>
 <?php
-/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+/*  Copyright 2012  Grant Kimball  (email : webmaster@yourlocalwebmaster.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
